8a2c0041a926835620860657eb5a1891573e8db2
David <u1918607@campus.udg.edu>
2019-05-01 23:57:00 +0200

    6

--- entrada.hs
+++ entrada.hs
@@ -13,7 +13,7 @@ import System.IO (hFlush, stdout)
 
 main = do
   let partida = iniciPartida
-  content <- readFile "pastor_copia.txt"
+  content <- readFile "pastor_copia.txt"--"pastor_copia.txt"
   let ls = words content
   mostrarTauler partida
   mostrarEntrada partida ls
@@ -39,14 +39,19 @@ main = do
 mostrarTauler :: Partida -> IO()
 mostrarTauler (Partida tauler color) = print tauler
 
+
+
+
 quedenParaules :: [String] -> Char
 quedenParaules [] = 'n'
 quedenParaules x = 'y'
 
 ferJugades :: Partida -> [String] -> (Partida,[String])
 ferJugades part@(Partida t c) [] = (part,[])
-ferJugades part@(Partida t c) [x,y] = ((Partida (fesJugada t (Jugada (Peca c (((\l->read[l]) (y!!0))) ((toInteger(digitToInt (y!!2))),(taulerCharToInteger (y!!1)))) (((toInteger(digitToInt (y!!4))),(taulerCharToInteger (y!!3)))))) (contrari c)),[])
-ferJugades part@(Partida t c) (x:y:z:xs) = ((Partida (fesJugada (jugAnterior part y) (Jugada (Peca (contrari c) (((\l->read[l]) (z!!0))) ((taulerCharToInteger (z!!1)),(toInteger(digitToInt (z!!2))))) (((taulerCharToInteger (z!!3)),(toInteger(digitToInt (z!!4))))))) c),xs)
+ferJugades part@(Partida t c) [x,y] = ((Partida (fesJugada t (Jugada (Peca c (((\l->read[l]) (y!!0))) ((toInteger(digitToInt (y!!2))),(taulerCharToInteger (y!!1)))) (((toInteger(digitToInt (y!!4))),(taulerCharToInteger (y!!3)))))) c),[])
+ferJugades part@(Partida t c) (x:y:z:xs) 
+  | isNumber (z!!0) = error "ferJugades: el fitxer d'entrada no és correcte"
+  | otherwise = ((Partida (fesJugada (jugAnterior part y) (Jugada (Peca (contrari c) (((\l->read[l]) (z!!0))) ((toInteger(digitToInt (z!!2)),(taulerCharToInteger (z!!1))))) (((toInteger(digitToInt (z!!4)),(taulerCharToInteger (z!!3))))))) c),xs)
  where
   jugAnterior (partida@(Partida t c)) n = fesJugada t (Jugada (Peca c (((\l->read[l]) (n!!0))) ((toInteger(digitToInt (n!!2))),(taulerCharToInteger (n!!1)))) (((toInteger(digitToInt (n!!4))),(taulerCharToInteger (n!!3))))) 
 
@@ -56,7 +61,7 @@ ferJugades part@(Partida t c) (x:y:z:xs) = ((Partida (fesJugada (jugAnterior par
 mostrarEntrada :: Partida -> [String] -> IO()
 mostrarEntrada part@(Partida t c) [] = print "mostrarEntrada: sha entrat a llista buida"
 mostrarEntrada part@(Partida t c) [x,y] = print "mostrarEntrada: s'ha entrat llista 2"
-mostrarEntrada part@(Partida t c) [x,y,z] = print "mostrarEntrada: s'ha entrat llista 3"
+--mostrarEntrada part@(Partida t c) [x,y,z] = print "mostrarEntrada: s'ha entrat llista 3"
 mostrarEntrada part@(Partida t c) (x:y:z:xs) = print "mostrarEntrada: sha entrat a llista plena, otherwise"
 
 
@@ -69,33 +74,23 @@ mostrarEntrada part@(Partida t c) (x:y:z:xs) = print "mostrarEntrada: sha entrat
 
 
 
+{- game :: Partida -> [String] -> IO ()
+game part@(Partida t c) ls = do
+    let partidaI = ferJugades part ls
+    mostrarTauler (fst partidaI)
+    prepSeguent (fst partidaI) (snd partidaI)
 
 
+prepSeguent :: Partida -> [String] -> IO ()
+prepSeguent part@(Partida t c) ls = do -}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{- segJug :: [String] -> [String]
+segJug [] = []
+segJug [x,y] = []
+segJug (x:y:z:xs)
+  | isNumber (z!!0) = (z:xs)
+  | otherwise = xs -}
 
 
 
